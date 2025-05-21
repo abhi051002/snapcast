09c2153c9270caf733f3af3074145ea6b0939c06
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-05-21 20:43:38 +0530

    rate limiting and some authetication added

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
+import aj from "./lib/arject";
+import { createMiddleware, detectBot, shield } from "@arcjet/next";
 
 export async function middleware(request: NextRequest, response: NextResponse) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
-    if (!session) {
-        return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
+
+const validate = aj.withRule(shield({ mode: "LIVE" })).withRule(
+  detectBot({
+    mode: "LIVE",
+    allow: ["CATEGORY:SEARCH_ENGINE", "G00G1E_CRAWLER"],
+  })
+);
+export default createMiddleware(validate);
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
+};
