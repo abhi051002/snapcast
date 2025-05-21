985820841b70ba9eb06f8b022894bec638b8cc74
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-05-21 20:44:49 +0530

    protected the video functionality

--- lib/actions/video.ts
+++ lib/actions/video.ts
@@ -7,91 +7,120 @@ import { BUNNY } from "@/constants";
 import { db } from "@/drizzle/db";
 import { videos } from "@/drizzle/schema";
 import { revalidatePath } from "next/cache";
+import aj from "../arject";
+import { fixedWindow, request } from "@arcjet/next";
 
 const VIDEO_STREAM_BASE_URL = BUNNY.STREAM_BASE_URL;
 const THUMBNAIL_STORGAE_BASE_URL = BUNNY.STORAGE_BASE_URL;
 const THUMBNAIL_CDN_URL = BUNNY.CDN_URL;
 const BUNNY_LIBRARY_ID = getEnv("BUNNY_LIBRARY_ID");
 const ACCESS_KEYS = {
-    streamAccessKey: getEnv("BUNNY_STREAM_ACCESS_KEY"),
-    storageAccessKey: getEnv("BUNNY_STORAGE_ACCESS_KEY"),
+  streamAccessKey: getEnv("BUNNY_STREAM_ACCESS_KEY"),
+  storageAccessKey: getEnv("BUNNY_STORAGE_ACCESS_KEY"),
 };
 // Helper Functions
 const getSeesionUserId = async (): Promise<string> => {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-    if (!session) {
-        throw new Error("Unauthenticated");
-    }
+  if (!session) {
+    throw new Error("Unauthenticated");
+  }
 
-    return session.user.id;
-}
+  return session.user.id;
+};
 
 const revalidatePaths = (paths: string[]) => {
-    paths.forEach((path) => revalidatePath(path));
+  paths.forEach((path) => revalidatePath(path));
+};
+
+// Validator Functions
+const validateWithArject = async (fingerPrint: string) => {
+  const rateLimit = aj.withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 2,
+      characteristics: ["fingerPrint"],
+    })
+  );
+
+  const req = await request();
+
+  const decision = await rateLimit.protect(req, { fingerPrint });
+
+  if (decision.isDenied()) {
+    throw new Error("Rate limit exceeded");
+  }
 };
 
 // Server Actions
 export const getVideoUploadUrl = withErrorHandling(async () => {
-    const getSessionUserId = await getSeesionUserId();
+  const getSessionUserId = await getSeesionUserId();
 
-    const videoResponse = await apiFetch<BunnyVideoResponse>(`${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos`, {
-        method: "POST",
-        bunnyType: "stream",
-        body: { title: "Temporrary Title", collectionId: '' },
-    });
-
-    const uploadUrl = `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoResponse.guid}`;
-    return {
-        videoId: videoResponse.guid,
-        uploadUrl,
-        accessKey: ACCESS_KEYS.streamAccessKey
+  const videoResponse = await apiFetch<BunnyVideoResponse>(
+    `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos`,
+    {
+      method: "POST",
+      bunnyType: "stream",
+      body: { title: "Temporrary Title", collectionId: "" },
     }
-})
+  );
 
+  const uploadUrl = `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoResponse.guid}`;
+  return {
+    videoId: videoResponse.guid,
+    uploadUrl,
+    accessKey: ACCESS_KEYS.streamAccessKey,
+  };
+});
 
-export const getThumbnailUploadUrl = withErrorHandling(async (videoId: string) => {
+export const getThumbnailUploadUrl = withErrorHandling(
+  async (videoId: string) => {
     const fileName = `${Date.now()}- ${videoId}-thumbnail`;
     const uploadUrl = `${THUMBNAIL_STORGAE_BASE_URL}/thumbnails/${fileName}`;
     const cdnUrl = `${THUMBNAIL_CDN_URL}/thumbnails/${fileName}`;
 
     return {
-        uploadUrl,
-        cdnUrl,
-        accessKey: ACCESS_KEYS.storageAccessKey
-    }
-})
+      uploadUrl,
+      cdnUrl,
+      accessKey: ACCESS_KEYS.storageAccessKey,
+    };
+  }
+);
 
-export const saveVideoDetails = withErrorHandling(async (videoDetails: VideoDetails) => {
+export const saveVideoDetails = withErrorHandling(
+  async (videoDetails: VideoDetails) => {
     const userId = await getSeesionUserId();
+    await validateWithArject(userId);
     await apiFetch(
-        `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoDetails.videoId}`,
-        {
-            method: "POST",
-            bunnyType: "stream",
-            body: {
-                title: videoDetails.title,
-                description: videoDetails.description
-            }
-        }
-    )
+      `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoDetails.videoId}`,
+      {
+        method: "POST",
+        bunnyType: "stream",
+        body: {
+          title: videoDetails.title,
+          description: videoDetails.description,
+        },
+      }
+    );
 
     await db.insert(videos).values({
-        videoId: videoDetails.videoId,
-        title: videoDetails.title,
-        description: videoDetails.description,
-        thumbnailUrl: videoDetails.thumbnailUrl,
-        visibility: videoDetails.visibility as "public" | "private",
-        duration: videoDetails.duration,
-        videoUrl: `${BUNNY.EMBED_URL}/${BUNNY_LIBRARY_ID}/${videoDetails.videoId}`,
-        userId,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+      videoId: videoDetails.videoId,
+      title: videoDetails.title,
+      description: videoDetails.description,
+      thumbnailUrl: videoDetails.thumbnailUrl,
+      visibility: videoDetails.visibility as "public" | "private",
+      duration: videoDetails.duration,
+      videoUrl: `${BUNNY.EMBED_URL}/${BUNNY_LIBRARY_ID}/${videoDetails.videoId}`,
+      userId,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     });
 
-    revalidatePaths(['/']);
+    revalidatePaths(["/"]);
 
-    return { videoId: videoDetails.videoId }
-})
+    return { videoId: videoDetails.videoId };
+  }
+);
