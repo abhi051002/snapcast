e71a2cfef2c2be4d1dc96d52890277da28e9ad4d
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-06-02 08:16:03 +0530

    add .env.example file and update .gitignore to exclude .env; add comment for ARJECT_API_KEY in arject.ts

--- lib/arject.ts
+++ lib/arject.ts
@@ -2,7 +2,7 @@ import arject from "@arcjet/next";
 import { getEnv } from "./utils";
 
 const aj = arject({
-  key: getEnv("ARJECT_API_KEY"),
+  key: getEnv("ARJECT_API_KEY"), // ajkey_01jvsm8d66fhd845m49grpsgj6
   rules: [],
 });
 
